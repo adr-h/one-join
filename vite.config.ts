3d3e13853500efd7867b975a25e7c60916d57b3b
adrian <adrianhyk@gmail.com>
2025-06-14 00:56:09 +0800

    swap back to swc again

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
-// use SWC (faster, but unstable on exotic platforms like Android)
-// import react from '@vitejs/plugin-react-swc';
+// use SWC (faster, but possibly unstable on exotic platforms like Termux)
+import react from '@vitejs/plugin-react-swc';
 
-// use Babel (slower, but rock-solid)
-import react from '@vitejs/plugin-react';
+// use Babel (slower, but more widely compatible)
+// import react from '@vitejs/plugin-react';
 
 import tailwindcss from '@tailwindcss/vite';
 
